e3a1f6912a95f3dee4cb7d1960b5c86f963c371f
María Eugenia Alfano <mariaeugeniaalfano@gmail.com>
2022-09-26 23:03:26 -0300

    correcciones en js

--- js/main.js
+++ js/main.js
@@ -1,65 +1,74 @@
-let producto = prompt("Escriba el nombre del producto que desea llevar. Tenemos disponible : Limpiador facial, Exfoliante, Tonico, Serum, Crema Hidratante y Protector Solar. ");
+debugger;
+let producto;
 let precio;
 let cantidad;
-const SUMA = precio * cantidad;
+let suma;
+const SALIR = "esc";
 
-debugger
-while (producto != "ESC") {
+while (producto != SALIR) {
+  producto = prompt(
+    "Escriba el nombre del producto que desea llevar. Tenemos disponible : Limpiador facial, Exfoliante, Tonico, Serum, Crema Hidratante y Protector Solar. Para salir escriba ´ESC´"
+  ).toLowerCase();
 
-    producto = prompt("Escriba el nombre del producto que desea llevar. Tenemos disponible : Limpiador facial, Exfoliante, Tonico, Serum, Crema Hidratante y Protector Solar. Para salir escriba ´ESC´");
+  switch (producto) {
+    case SALIR:
+      break;
 
-    switch (producto) {
-        case "Limpiador facial":
-            precio = 1240;
-            alert("Usted eligió: Limpiador Facial. Su valor es de $" + precio);
-            cantidad = parseInt(prompt("¿Cuantos desea comprar?"));
-            alert("el total es $" + (precio * cantidad));
-            confirm("¿Desea seguir comprando?");
-            break;
+    case "limpiador facial":
+      precio = 1240;
+      alert("Usted eligió: Limpiador Facial. Su valor es de $" + precio);
+      cantidad = parseInt(prompt("¿Cuantos desea comprar?"));
+      suma = precio * cantidad;
+      alert("el total es $" + suma);
+      break;
 
-        case "Exfoliante":
-            precio = 1120;
-            alert("Usted eligió: Exfoliante. Su valor es de $" + precio)
-            cantidad = parseInt(prompt("¿Cuantos desea comprar?"));
-            alert("el total es $" + (precio * cantidad));
-            confirm("¿Desea seguir comprando?");
-            break;
+    case "exfoliante":
+      precio = 1120;
+      alert("Usted eligió: Exfoliante. Su valor es de $" + precio);
+      cantidad = parseInt(prompt("¿Cuantos desea comprar?"));
+      suma = precio * cantidad;
+      alert("el total es $" + suma);
+      break;
 
-        case "Tonico":
-            precio = 1450;
-            alert("Usted eligió: Tonico. Su valor es de $" + precio)
-            cantidad = parseInt(prompt("¿Cuantos desea comprar?"));
-            alert("el total es $" + (precio * cantidad));
-            confirm("¿Desea seguir comprando?");
-            break;
+    case "tonico":
+      precio = 1450;
+      alert("Usted eligió: Tonico. Su valor es de $" + precio);
+      cantidad = parseInt(prompt("¿Cuantos desea comprar?"));
+      suma = precio * cantidad;
+      alert("el total es $" + suma);
+      break;
 
-        case "Serum":
-            precio = 1050;
-            alert("Usted eligió: Serum . Su valor es de $" + precio)
-            cantidad = parseInt(prompt("¿Cuantos desea comprar?"));
-            alert("el total es $" + (precio * cantidad));
-            confirm("¿Desea seguir comprando?");
-            break;
+    case "serum":
+      precio = 1050;
+      alert("Usted eligió: Serum . Su valor es de $" + precio);
+      cantidad = parseInt(prompt("¿Cuantos desea comprar?"));
+      suma = precio * cantidad;
+      alert("el total es $" + suma);
+      break;
 
-        case "Crema Hidratante":
-            precio = 1400;
-            alert("Usted eligió: Crema Hidratante . Su valor es de $" + precio)
-            cantidad = parseInt(prompt("¿Cuantos desea comprar?"));
-            alert("el total es $" + (precio * cantidad));
-            confirm("¿Desea seguir comprando?");
-            break;
+    case "crema hidratante":
+      precio = 1400;
+      alert("Usted eligió: Crema Hidratante . Su valor es de $" + precio);
+      cantidad = parseInt(prompt("¿Cuantos desea comprar?"));
+      suma = precio * cantidad;
+      alert("el total es $" + suma);
+      break;
 
-        case "Protector Solar":
-            precio = 2500;
-            alert("Usted eligió: Protector Solar . Su valor es de $" + precio)
-            cantidad = parseInt(prompt("¿Cuantos desea comprar?"));
-            alert("el total es $" + (precio * cantidad));
-            confirm("¿Desea seguir comprando?");
-            break;
+    case "protector solar":
+      precio = 2500;
+      alert("Usted eligió: Protector Solar . Su valor es de $" + precio);
+      cantidad = parseInt(prompt("¿Cuantos desea comprar?"));
+      suma = precio * cantidad;
+      alert("el total es $" + suma);
+      break;
 
-        default:
-            alert("⛔ Lo siento, intenta de nuevo");
-            break;
-    }
+    default:
+      alert("⛔ Lo siento, intenta de nuevo");
+      break;
+  }
 
+  let result = confirm("¿Desea seguir comprando?");
+  if (!result) {
+    break;
+  }
 }
