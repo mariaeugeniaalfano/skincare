0aa269d16535a0580cd8f47f4a92b807bd68599c
maria eugenia alfano <mariaeugeniaalfano@gmail.com>
2022-10-13 15:14:33 -0300

    se agrego contenido a "productos" para aplicar dom

--- js/main.js
+++ js/main.js
@@ -1,70 +1,65 @@
-//debugger;
-let producto = "";
-let precio = 0;
-let cantidad = 0;
-let suma = 0;
+let producto;
+let precio;
+let cantidad;
+let suma;
 let sumaTotal = 0;
 const SALIR = "esc";
 
 function compra(precio, producto) {
-  alert(
-    "Usted eligió: " + producto + ". El valor de este producto es de $" + precio
-  );
-  cantidad = parseInt(prompt("¿Cuantos desea comprar?"));
-  suma = precio * cantidad;
-  alert("El total de los productos seleccionados es de $" + suma);
-  sumaTotal += suma;
+    alert("Usted eligió:" + producto + ". Su valor es de $" + precio);
+    cantidad = parseInt(prompt("¿Cuantos desea comprar?"));
+    suma = precio * cantidad;
+    alert("el total es $" + suma);
+    sumaTotal += suma;
 }
 
 while (producto != SALIR) {
-  producto = prompt(
-    `Escriba el nombre del producto que desea llevar. Tenemos disponible :
-    *Limpiador facial 
-    *Exfoliante 
-    *Tonico 
-    *Serum 
-    *Crema Hidratante 
-    *Protector Solar. 
-  Para salir escriba 'ESC'`
-  ).toLowerCase();
+    producto = prompt(
+        'Escriba el nombre del producto que desea llevar. Tenemos disponible : Limpiador facial, Exfoliante, Tonico, Serum, Crema Hidratante y Protector Solar. Para salir escriba ´ESC´'
+    ).toLowerCase();
 
-  switch (producto) {
-    case SALIR:
-      break;
+    switch (producto) {
+        case SALIR:
+            break;
 
-    case "limpiador facial":
-      compra(1240, "Limpiador Facial");
-      break;
+        case "limpiador facial":
+            compra(1240, 'limpiador facial');
+            break;
 
-    case "exfoliante":
-      compra(1120, "Exfoliante");
-      break;
+        case "exfoliante":
+            compra(1120, 'exfoliante');
+            break;
 
-    case "tonico":
-      compra(1450, "Tónico");
-      break;
+        case "tonico":
+            compra(1450, 'tonico');
+            break;
 
-    case "serum":
-      compra(1050, "Serum");
-      break;
+        case "serum":
+            compra(1050, 'serum');
+            break;
 
-    case "crema hidratante":
-      compra(1400, "Crema Hidratante");
-      break;
+        case "crema hidratante":
+            compra(1400, 'crema hidratante');
+            break;
 
-    case "protector solar":
-      compra(2500, "Protector Solar");
-      break;
+        case "protector solar":
+            compra(2500, 'protector solar');
+            break;
 
-    default:
-      alert("⛔ Lo siento, intenta de nuevo");
-      break;
-  }
+        default:
+            alert("⛔ Lo siento, intenta de nuevo");
+            break;
+    }
 
-  let result = confirm("¿Desea seguir comprando?");
-  if (!result) {
-    alert("La suma acumulada al momento es: $" + sumaTotal);
-    alert("¡Gracias por su compra");
-    break;
-  }
+    if (producto !== SALIR) {
+        let result = confirm("¿Desea seguir comprando?");
+
+        if (!result) {
+
+            alert("la suma acumulada al momento es: " + sumaTotal);
+            alert("¡Gracias por su compra!")
+            break;
+        }
+    }
+
 }
