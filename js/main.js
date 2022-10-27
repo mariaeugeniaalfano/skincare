93012050aab5a2f8b506946fec035cd97615ca4b
maria eugenia alfano <mariaeugeniaalfano@gmail.com>
2022-10-27 08:37:59 -0300

    arreglo push array

--- js/main.js
+++ js/main.js
@@ -1,147 +1,158 @@
+//variables HTML DOM
+let limpiador = document.getElementById("input_limpiador");
+let exfoliante = document.getElementById("input_exfoliante");
+let tonico = document.getElementById("input_tonico");
+let serum = document.getElementById("input_serum");
+let cremHidra = document.getElementById("input_cremahidratante");
+let protectSolar = document.getElementById("input_protectorsolar");
+let vaciarChanguito = document.getElementById("id_vaciarChanguito");
+let verChanguito = document.getElementById("id_verChanguito");
+
+vaciarChanguito.addEventListener("click", vaciarchango);
+verChanguito.addEventListener("click", verProductosEnChanguito);
+
+//botones DOM
 let botonLimpiador = document.getElementById("id_limpiador");
+botonLimpiador.addEventListener("click", agregarChanguito);
+
 let botonExfoliante = document.getElementById("id_exfoliante");
+botonExfoliante.addEventListener("click", agregarChanguito);
+
 let botonTonico = document.getElementById("id_tonico");
+botonTonico.addEventListener("click", agregarChanguito);
+
 let botonSerum = document.getElementById("id_serum");
-let botonCremHidra = document.getElementById("id_cremahidratante");
-let botonProtectSolar = document.getElementById("id_protectorsolar");
+botonSerum.addEventListener("click", agregarChanguito);
 
-function llamado(boton) {
-  boton.addEvenLisstener("click", )
-}
+let botonCremaHidra = document.getElementById("id_cremahidratante");
+botonCremaHidra.addEventListener("click", agregarChanguito);
 
-botonLimpiador.addEvenLisstener("Click", ) = () => {
-  console.log("seleccionaste limpiador");
-};
+let botonProtectorSolar = document.getElementById("id_protectorsolar");
+botonProtectorSolar.addEventListener("click", agregarChanguito);
 
-botonExfoliante.onclick = () => {
-  console.log("seleccionaste exfoliante");
-};
-
-botonTonico.onclick = () => {
-  console.log("seleccionaste tonico");
-};
-
-botonSerum.onclick = () => {
-  console.log("seleccionaste serum");
-};
-
-botonCremHidra.onclick = () => {
-  console.log("seleccionaste Crema Hidratante");
-};
-
-botonProtectSolar.onclick = () => {
-  console.log("seleccionaste Protector Solar");
-};
-
-// variables
-let producto;
-let precio;
-let cantidadProductos;
-let suma;
-let sumaTotal = 0;
-const SALIR = "esc";
-
-//clase que pasa nombre y precio
-class Producto {
-  constructor(nombre, precio) {
-    this.nombre = nombre;
-    this.precio = precio;
-  }
-}
+//variables precios productos
+let precioLimpiador = 1240;
+let precioExfoliante = 1120;
+let precioTonico = 1450;
+let precioSerum = 1050;
+let precioCremaHidra = 1400;
+let precioProtectSol = 2500;
 
 //array que carga los productos seleccionados
-const ARRAY_PRODUCTOS = [];
+let ARRAY_PRODUCTOS = [];
 
-//funcion para pedir producto y calcular total
-function compra(precio, producto) {
-  alert(
-    "Usted eligió: " + producto + ". El valor del producto es de $" + precio
-  );
-  cantidadProductos = parseInt(prompt("¿Cuantos desea comprar?"));
-  suma = precio * cantidadProductos;
-  alert(
-    "El total por " + cantidadProductos + " " + producto + " es de $" + suma
-  );
-  sumaTotal += suma;
+//------------------------------------------------------//
+
+//STORAGE
+let dataStorage = JSON.parse(localStorage.getItem("carrito"));
+if (dataStorage !== null) {
+  verProductosEnChanguito();
 }
 
-//while con switch que carga productos seleccionados al array
-while (producto != SALIR) {
-  producto = prompt(
-    "Escriba el nombre del producto que desea llevar. Tenemos disponible : \n*Limpiador facial \n*Exfoliante \n*Tonico \n*Serum \n*Crema Hidratante \n*Protector Solar. \nPara salir escriba ´ESC´"
-  ).toLowerCase();
+//clase que pasa nombre y precio de cada producto
+class Producto {
+  constructor(name, value, precioProducto) {
+    this.name = name;
+    this.value = value;
+    this.precioProducto = precioProducto;
+  }
+}
 
-  switch (producto) {
-    case SALIR:
-      break;
+// funcion crear producto con push al array
+const crearProducto = (name, value, precioProducto) => {
+  let producto = new Producto(name, value, precioProducto);
 
-    case "limpiador facial":
-      let limpiadorFacial = new Producto(
-        "limpiador facial",
-        1240,
-        cantidadProductos
-      );
-      compra(limpiadorFacial.precio, limpiadorFacial.nombre);
-      ARRAY_PRODUCTOS.push(limpiadorFacial);
-      break;
-
-    case "exfoliante":
-      let exfoliante = new Producto("exfoliante", 1120, cantidadProductos);
-      compra(exfoliante.precio, exfoliante.nombre);
-      ARRAY_PRODUCTOS.push(exfoliante);
-      break;
-
-    case "tonico":
-      let tonico = new Producto("tonico", 1450, cantidadProductos);
-      compra(tonico.precio, tonico.nombre);
-      ARRAY_PRODUCTOS.push(tonico);
-      break;
-
-    case "serum":
-      let serum = new Producto("serum", 1050, cantidadProductos);
-      compra(serum.precio, serum.nombre);
-      ARRAY_PRODUCTOS.push(serum);
-      break;
-
-    case "crema hidratante":
-      let cremaHidratante = new Producto(
-        "crema hidratante",
-        1400,
-        cantidadProductos
-      );
-      compra(cremaHidratante.precio, cremaHidratante.nombre);
-      ARRAY_PRODUCTOS.push(cremaHidratante);
-      break;
-
-    case "protector solar":
-      let protectorSolar = new Producto(
-        "protector solar",
-        2500,
-        cantidadProductos
-      );
-      compra(protectorSolar.precio, protectorSolar.nombre);
-      ARRAY_PRODUCTOS.push(protectorSolar);
-      break;
-
-    default:
-      alert("⛔ Lo siento, intenta de nuevo");
-      break;
+  if (ARRAY_PRODUCTOS.some((x) => x.name == producto.name)) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: "Ya tiene ese producto en el carrito, no puede volver a agregarlo.",
+    });
+    return;
   }
 
-  //if que confirma si usuario quiere salir y devuelve array + cantidad a abonar
-  if (producto != SALIR) {
-    let result = confirm("¿Desea seguir comprando?");
+  ARRAY_PRODUCTOS.push(producto);
+  localStorage.setItem("carrito", JSON.stringify(ARRAY_PRODUCTOS));
+  mostarMensajeProductoAgregado(producto.name);
+};
 
-    if (!result) {
-      for (let i = 0; i <= ARRAY_PRODUCTOS.length; i++) {
-        alert(
-          "Producto seleccionado: " +
-            ARRAY_PRODUCTOS[i].nombre.toUpperCase() +
-            "." +
-            "\n\n✅La suma TOTAL de la compra es de : $" +
-            sumaTotal
-        );
-      }
+//funcion agregar changuito
+function agregarChanguito() {
+  if (limpiador.value > 0) {
+    crearProducto(limpiador.name, limpiador.value, precioLimpiador);
+  } else if (exfoliante.value > 0) {
+    crearProducto(exfoliante.name, exfoliante.value, precioExfoliante);
+  } else if (tonico.value > 0) {
+    crearProducto(tonico.name, tonico.value, precioTonico);
+  } else if (serum.value > 0) {
+    crearProducto(serum.name, serum.value, precioSerum);
+  } else if (cremHidra.value > 0) {
+    crearProducto(cremHidra.name, cremHidra.value, precioCremaHidra);
+  } else if (protectSolar.value > 0) {
+    crearProducto(protectSolar.name, protectSolar.value, precioProtectSol);
+  }
+}
+
+//Sweet Alert "agregado al carrito"
+function mostarMensajeProductoAgregado(nombreProducto) {
+  Swal.fire({
+    position: "top-end",
+    icon: "success",
+    title: "Se ha agregado " + nombreProducto + " al carrito",
+    showConfirmButton: false,
+    timer: 2000,
+  });
+}
+
+//Vaciar changuito
+function vaciarchango() {
+  if (dataStorage !== null) {
+    localStorage.removeItem("carrito");
+    ARRAY_PRODUCTOS = [];
+    Swal.fire({
+      icon: "error",
+      title: "Se ha vaciado el carrito",
+      text: "0 productos en el carrito",
+    });
+  }
+}
+
+//Ver changuito
+function verProductosEnChanguito() {
+  dataStorage = JSON.parse(localStorage.getItem("carrito"));
+
+  if (dataStorage !== null) {
+    let sumatoriaTotalProductos = 0;
+    let mensajeGenerado = "Tu carrito tiene:\n";
+
+    for (let i = 0; i < dataStorage.length; i++) {
+
+
+      mensajeGenerado += "\n" + dataStorage[i].value + " " + dataStorage[i].name + ". Precio por unidad: $" + dataStorage[i].precioProducto + "\n"
+
+      sumatoriaTotalProductos +=
+        parseInt(dataStorage[i].value) * dataStorage[i].precioProducto;
     }
+    mensajeGenerado += "\n TOTAL: $" + sumatoriaTotalProductos;
+
+    Swal.fire({
+      title: mensajeGenerado,
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+    });
+  } else {
+    Swal.fire({
+      title: "No hay productos en el changuito",
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+    });
   }
 }
